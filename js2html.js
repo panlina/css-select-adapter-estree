191aecf9bee84ec4b98eda4a201b33ad35924056
Jack Lu <jacklu@jacklu.me>
2017-05-29 15:56:57 +0800

    +statement, +function expression

--- js2html.js
+++ js2html.js
@@ -8,6 +8,27 @@
 			case 'Identifier':
 				return $("<identifier>").append(js.name);
 				break;
+			case 'FunctionExpression':
+				return $("<function>").append(statement(js.body));
+				break;
 		}
+	}
+	function statement(js) {
+		switch (js.type) {
+			case "ReturnStatement":
+				return $("<return>").append(expression(js.argument));
+				break;
+			case "VariableDeclaration":
+				return $("<var>").append(js.declarations.map(decl))
+				break;
+			case "BlockStatement":
+				return $("<block>").append(js.body.map(statement));
+				break;
+		}
+	}
+	function decl(js) {
+		return $("<decl>")
+			.append($("<name>").text(js.id.name))
+			.append($("<value>").append(expression(js.init)));
 	}
 }
