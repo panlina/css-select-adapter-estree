e5a2340c7625d28edc6c5c4f3695185ffd14d8ba
Jack Lu <jacklu@jacklu.me>
2017-05-30 17:50:00 +0800

    +identifier, statement, function

--- html2js.js
+++ html2js.js
@@ -9,6 +9,49 @@
 					value: eval(html.html())
 				}
 				break;
+			case 'IDENTIFIER':
+				return {
+					type: 'Identifier',
+					name: html.html()
+				};
+				break;
+			case 'FUNCTION':
+				return {
+					type: 'FunctionExpression',
+					params: [],
+					body: statement(html.children())
+				};
+				break;
 		}
+	}
+	function statement(html) {
+		switch (html[0].tagName) {
+			case 'RETURN':
+				return {
+					type: 'ReturnStatement',
+					argument: expression(html.children())
+				};
+				break;
+			case 'VAR':
+				return {
+					type: 'VariableDeclaration',
+					kind: 'var',
+					declarations: html.children().map(function () { return decl($(this)); })
+				};
+				break;
+			case 'BLOCK':
+				return {
+					type: 'BlockStatement',
+					body: html.children().map(function () { return statement($(this)); })
+				};
+				break;
+		}
+	}
+	function decl(html) {
+		return {
+			type: 'VariableDeclarator',
+			id: { type: 'Identifier', name: html.children('name').html() },
+			init: expression(html.children('value').children())
+		};
 	}
 }
