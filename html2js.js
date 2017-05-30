03aea11101f5c835794d25f6e2b88629db15020a
Jack Lu <jacklu@jacklu.me>
2017-05-30 14:10:57 +0800

    +html2js

---
+++ html2js.js
@@ -0,0 +1,14 @@
+﻿function html2js(html) {
+	return expression(html);
+	function expression(html) {
+		if (html == undefined) return undefined;
+		switch (html[0].tagName) {
+			case 'LITERAL':
+				return {
+					type: 'Literal',
+					value: eval(html.html())
+				}
+				break;
+		}
+	}
+}
