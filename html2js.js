d51da47b3b4a70e881c023a6c7fc505ec749f55b
Jack Lu <jacklu@jacklu.me>
2017-06-05 00:02:46 +0800

    html2js: fix: expression([])=undefined

--- html2js.js
+++ html2js.js
@@ -1,7 +1,7 @@
 ﻿function html2js(html) {
 	return expression(html);
 	function expression(html) {
-		if (html == undefined) return undefined;
+		if (html[0] == undefined) return undefined;
 		switch (html[0].tagName) {
 			case 'LITERAL':
 				return {
