48fb45f96ca39e342d66ab402178f938d61edf71
Jack Lu <jacklu@jacklu.me>
2017-07-21 22:20:37 +0800

    merge expression, statement

--- html2js.js
+++ html2js.js
@@ -2,101 +2,92 @@
 var syntax = require('./syntax.js').syntax;
 var Syntax = require('./syntax.js').Syntax;
 function html2js(html) {
-	return expression(html);
-	function expression(html) {
-		if (html[0] == undefined) return undefined;
-		switch (html[0].tagName) {
-			case 'literal':
-				return {
-					type: 'Literal',
-					value: eval(html.html())
-				}
-				break;
-			case 'identifier':
-				return {
-					type: 'Identifier',
-					name: html.html()
-				};
-				break;
-			case 'call':
-				return {
-					type: 'CallExpression',
-					callee: expression(html.children('callee').children()),
-					arguments: html.children('arguments').children().map(function () { return expression($(this)); })
-				};
-				break;
-			case 'member':
-				return translate(syntax.member)(html);
-				break;
-			case 'function':
-				return {
-					type: 'FunctionExpression',
-					params: html.children('params').children().map(function () { return expression($(this)); }),
-					body: statement(html.children('body').children())
-				};
-				break;
-			case 'unary':
-				var js = translate(syntax.unary)(html);
-				if (js.operator == '++' || js.operator == '--')
-					js.type = 'UpdateExpression';
-				return js;
-				break;
-			case 'binary':
-				return translate(syntax.binary)(html);
-				break;
-		}
-	}
-	function statement(html) {
-		if (html[0] == undefined) return undefined;
-		var js;
-		switch (html[0].tagName) {
-			case 'expression':
-				js = {
-					type: 'ExpressionStatement',
-					expression: expression(html.children())
-				};
-				break;
-			case 'return':
-				js = {
-					type: 'ReturnStatement',
-					argument: expression(html.children())
-				};
-				break;
-			case 'var':
-				js = {
-					type: 'VariableDeclaration',
-					kind: 'var',
-					declarations: html.children().map(function () { return statement($(this)); })
-				};
-				break;
-			case 'decl':
-				js = translate(syntax.decl)(html);
-				break;
-			case 'block':
-				js = {
-					type: 'BlockStatement',
-					body: html.children().map(function () { return statement($(this)); })
-				};
-				break;
-			case 'if':
-				js = translate(syntax.if)(html);
-				break;
-			case 'while':
-				js = translate(syntax.while)(html);
-				break;
-			case 'for':
-				js = translate(syntax.for)(html);
-				break;
-		}
-		var label = html.attr('label');
-		if (label)
+	if (html[0] == undefined) return undefined;
+	var js;
+	switch (html[0].tagName) {
+		case 'literal':
 			js = {
-				type: 'LabeledStatement',
-				label: { type: 'Identifier', name: label },
-				body: js
+				type: 'Literal',
+				value: eval(html.html())
+			}
+			break;
+		case 'identifier':
+			js = {
+				type: 'Identifier',
+				name: html.html()
 			};
-		return js;
+			break;
+		case 'call':
+			js = {
+				type: 'CallExpression',
+				callee: html2js(html.children('callee').children()),
+				arguments: html.children('arguments').children().map(function () { return html2js($(this)); })
+			};
+			break;
+		case 'member':
+			js = translate(syntax.member)(html);
+			break;
+		case 'function':
+			js = {
+				type: 'FunctionExpression',
+				params: html.children('params').children().map(function () { return html2js($(this)); }),
+				body: html2js(html.children('body').children())
+			};
+			break;
+		case 'unary':
+			js = translate(syntax.unary)(html);
+			if (js.operator == '++' || js.operator == '--')
+				js.type = 'UpdateExpression';
+			break;
+		case 'binary':
+			js = translate(syntax.binary)(html);
+			break;
+		case 'expression':
+			js = {
+				type: 'ExpressionStatement',
+				expression: html2js(html.children())
+			};
+			break;
+		case 'return':
+			js = {
+				type: 'ReturnStatement',
+				argument: html2js(html.children())
+			};
+			break;
+		case 'var':
+			js = {
+				type: 'VariableDeclaration',
+				kind: 'var',
+				declarations: html.children().map(function () { return html2js($(this)); })
+			};
+			break;
+		case 'decl':
+			js = translate(syntax.decl)(html);
+			break;
+		case 'block':
+			js = {
+				type: 'BlockStatement',
+				body: html.children().map(function () { return html2js($(this)); })
+			};
+			break;
+		case 'if':
+			js = translate(syntax.if)(html);
+			break;
+		case 'while':
+			js = translate(syntax.while)(html);
+			break;
+		case 'for':
+			js = translate(syntax.for)(html);
+			break;
 	}
+	var label = html.attr('label');
+	if (label)
+		js = {
+			type: 'LabeledStatement',
+			label: { type: 'Identifier', name: label },
+			body: js
+		};
+	return js;
 	function translate(syntax) {
 		return function (html) {
 			var js = {};
@@ -104,8 +95,8 @@ function html2js(html) {
 			for (var name in syntax.property) {
 				var type = syntax.property[name] || Syntax.type(html.children(name).children());
 				js[name] = {
-					expression: expression,
-					statement: statement,
+					expression: html2js,
+					statement: html2js,
 					string: function (x) { return x.text(); },
 					bool: function (x) { return x.text() != 'false'; }
 				}[type](html.children(name).contents())
--- js2html.js
+++ js2html.js
@@ -2,80 +2,72 @@
 var syntax = require('./syntax.js').syntax;
 var Syntax = require('./syntax.js').Syntax;
 function js2html(js) {
-	return expression(js);
-	function expression(js) {
-		if (js == undefined) return undefined;
-		switch (js.type) {
-			case 'Literal':
-				return $("<literal>").append(js.raw);
-				break;
-			case 'Identifier':
-				return $("<identifier>").append(js.name);
-				break;
-			case 'CallExpression':
-				return $("<call>").append(
-					$("<callee>").append(
-						expression(js.callee)
-					),
-					$("<arguments>").append(
-						js.arguments.map(expression)
-					)
-				);
-				break;
-			case 'MemberExpression':
-				return translate(syntax.member)(js);
-				break;
-			case 'FunctionExpression':
-				return $("<function>").append(
-					$("<params>").append(
-						js.params.map(expression)
-					),
-					$("<body>").append(
-						statement(js.body)
-					)
-				);
-				break;
-			case 'UnaryExpression':
-			case 'UpdateExpression':
-				return translate(syntax.unary)(js);
-				break;
-			case 'BinaryExpression':
-			case 'AssignmentExpression':
-				return translate(syntax.binary)(js);
-				break;
-		}
-	}
-	function statement(js) {
-		if (js == undefined) return undefined;
-		switch (js.type) {
-			case "ExpressionStatement":
-				return $("<expression>").append(expression(js.expression));
-				break;
-			case "LabeledStatement":
-				return statement(js.body).attr('label', js.label.name);
-				break;
-			case "ReturnStatement":
-				return $("<return>").append(expression(js.argument));
-				break;
-			case "VariableDeclaration":
-				return $("<var>").append(js.declarations.map(statement))
-				break;
-			case "VariableDeclarator":
-				return translate(syntax.decl)(js);
-				break;
-			case "BlockStatement":
-				return $("<block>").append(js.body.map(statement));
-				break;
-			case "IfStatement":
-				return translate(syntax.if)(js);
-				break;
-			case "WhileStatement":
-				return translate(syntax.while)(js);
-				break;
-			case "ForStatement":
-				return translate(syntax.for)(js);
-				break;
-		}
+	if (js == undefined) return undefined;
+	switch (js.type) {
+		case 'Literal':
+			return $("<literal>").append(js.raw);
+			break;
+		case 'Identifier':
+			return $("<identifier>").append(js.name);
+			break;
+		case 'CallExpression':
+			return $("<call>").append(
+				$("<callee>").append(
+					js2html(js.callee)
+				),
+				$("<arguments>").append(
+					js.arguments.map(js2html)
+				)
+			);
+			break;
+		case 'MemberExpression':
+			return translate(syntax.member)(js);
+			break;
+		case 'FunctionExpression':
+			return $("<function>").append(
+				$("<params>").append(
+					js.params.map(js2html)
+				),
+				$("<body>").append(
+					js2html(js.body)
+				)
+			);
+			break;
+		case 'UnaryExpression':
+		case 'UpdateExpression':
+			return translate(syntax.unary)(js);
+			break;
+		case 'BinaryExpression':
+		case 'AssignmentExpression':
+			return translate(syntax.binary)(js);
+			break;
+		case "ExpressionStatement":
+			return $("<expression>").append(js2html(js.expression));
+			break;
+		case "LabeledStatement":
+			return js2html(js.body).attr('label', js.label.name);
+			break;
+		case "ReturnStatement":
+			return $("<return>").append(js2html(js.argument));
+			break;
+		case "VariableDeclaration":
+			return $("<var>").append(js.declarations.map(js2html))
+			break;
+		case "VariableDeclarator":
+			return translate(syntax.decl)(js);
+			break;
+		case "BlockStatement":
+			return $("<block>").append(js.body.map(js2html));
+			break;
+		case "IfStatement":
+			return translate(syntax.if)(js);
+			break;
+		case "WhileStatement":
+			return translate(syntax.while)(js);
+			break;
+		case "ForStatement":
+			return translate(syntax.for)(js);
+			break;
 	}
 	function translate(syntax) {
 		return function (js) {
@@ -83,8 +75,8 @@ function js2html(js) {
 				Object.keys(syntax.property).map(function (name) {
 					var type = syntax.property[name] || Syntax.type(js[name]);
 					return $('<' + name + '>').append({
-						expression: expression,
-						statement: statement,
+						expression: js2html,
+						statement: js2html,
 						string: function (x) { return x.toString();},
 						bool: function (x) { return x.toString();}
 					}[type](js[name]));
