4663f1289e39df21a5201163753e34fb71a3f38f
Jack Lu <jacklu@jacklu.me>
2017-11-16 15:51:00 +0800

    +more tests

--- test.js
+++ test.js
@@ -5,7 +5,17 @@ var js = acorn.parse(js);
 var adapter = require('.').adapter;
 var Node = require('.').Node;
 var select = require('css-select');
-var element = select(":root>.var>decl", new Node(js), { adapter: adapter });
 var escodegen = require("escodegen");
-for (var i in element)
-	console.log(escodegen.generate(element[i].value));
+var node = new Node(js);
+var selector = [
+	":root>.var>decl",
+	"[label]",
+	".unary[prefix]",
+	".func>#body>:last-child"
+];
+for (var i in selector) {
+	console.log('\x1b[1m', selector[i], '\x1b[0m');
+	var element = select(selector[i], node, { adapter: adapter });
+	for (var i in element)
+		console.log(escodegen.generate(element[i].value));
+}
