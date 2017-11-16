35b03259a03685cf318bf3881498fb8e515a0529
Jack Lu <jacklu@jacklu.me>
2017-11-16 23:55:35 +0800

    manipulation: +after

--- manipulation.js
+++ manipulation.js
@@ -12,7 +12,22 @@ function before(syntax) {
 	var i = parent.getChildren().indexOf(this);
 	parent.getChildren().splice(i, 0, node);
 }
+function after(syntax) {
+	var parent = this.parent;
+	for (var name in parent.syntax.property) {
+		var p = parent.syntax.property[name];
+		if (p.name == this.property)
+			break;
+	}
+	var i = parent.value[name].indexOf(this.value);
+	parent.value[name].splice(i + 1, 0, syntax);
+	var node = new this.constructor(syntax);
+	node.parent = parent;
+	var i = parent.getChildren().indexOf(this);
+	parent.getChildren().splice(i + 1, 0, node);
+}
 
 module.exports = {
-	before: before
+	before: before,
+	after: after
 };
