4a3baddc2e1dd96b43fcf3c96f712391358dc255
Jack Lu <jacklu@jacklu.me>
2017-11-17 23:56:25 +0800

    manipulation: +append, prepend

--- manipulation.js
+++ manipulation.js
@@ -26,8 +26,34 @@ function after(syntax) {
 	var i = parent.getChildren().indexOf(this);
 	parent.getChildren().splice(i + 1, 0, node);
 }
+function append(syntax, property) {
+	for (var name in this.syntax.property) {
+		var p = this.syntax.property[name];
+		if (p.name == property)
+			break;
+	}
+	this.value[name].push(syntax);
+	var node = new this.constructor(syntax);
+	node.parent = this;
+	node.property = property;
+	this.getChildren().push(node);
+}
+function prepend(syntax, property) {
+	for (var name in this.syntax.property) {
+		var p = this.syntax.property[name];
+		if (p.name == property)
+			break;
+	}
+	this.value[name].unshift(syntax);
+	var node = new this.constructor(syntax);
+	node.parent = this;
+	node.property = property;
+	this.getChildren().unshift(node);
+}
 
 module.exports = {
 	before: before,
-	after: after
+	after: after,
+	append: append,
+	prepend: prepend
 };
