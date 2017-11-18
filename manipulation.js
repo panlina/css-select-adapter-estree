c9716ee6930e31d6a57a86d1a7ced3f5771d8cdd
Jack Lu <jacklu@jacklu.me>
2017-11-19 00:08:25 +0800

    +replace

--- manipulation.js
+++ manipulation.js
@@ -67,11 +67,31 @@ function remove() {
 	} else if (p.type == '{}')
 		parent.value[name] = null;
 }
+function replace(syntax) {
+	var parent = this.parent;
+	for (var name in parent.syntax.property) {
+		var p = parent.syntax.property[name];
+		if (p.name == this.property)
+			break;
+	}
+	if (typeof (p) == 'string')
+		p = { name: name, type: p };
+	if (p.type == '[]') {
+		var i = parent.value[name].indexOf(this.value);
+		parent.value[name][i] = syntax;
+	} else if (p.type == '{}')
+		parent.value[name] = syntax;
+	var node = new this.constructor(syntax);
+	node.parent = parent;
+	var i = parent.getChildren().indexOf(this);
+	parent.getChildren()[i] = node;
+}
 
 module.exports = {
 	before: before,
 	after: after,
 	append: append,
 	prepend: prepend,
-	remove: remove
+	remove: remove,
+	replace: replace
 };
